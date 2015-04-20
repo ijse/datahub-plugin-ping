63852b22fc0d81d26cd948c4013c24f889590558
ijse <i@ijser.cn>
2015-04-21 01:38:08 +0800

    add comments for plugin code

--- index.js
+++ index.js
@@ -1,24 +1,73 @@
 
+
+/**
+ * Default plugin configs
+ *   read by pluginLoader for initializing the plugin
+ *
+ * @type {object}
+ */
+Ping.config = require('./config');
+
+/**
+ * Plugin Object
+ *
+ * this.route:
+ *     `express.Router()` instance under the url path `/{pluginName}`
+ *
+ * this.logger:
+ *     function to output debug messages
+ *
+ * @param {object} config plugin configurations
+ */
 function Ping(config) {
 
-  var _this = this;
-  _this.greeting = config.greeting;
+  // custom plugin attribute
+  this.greeting = config.greeting;
 
   this.route.get('/', function(req, res) {
-    res.end('Pong! ' + _this.greeting + '\n');
-  });
+    res.end('Pong! ' + this.greeting + '\n');
+  }.bind(this));
 
+  // custom plugin listener
+  //  for all messages under the scope of `plugin:{pluginName}:*`
+  //
+  // eg.:
+  //
+  // ```
+  //    hub.emit('plugin:ping:setGreeting', 'hello');
+  // ```
+  //
+  // which will be observed by:
+  //
+  // ```
+  //    this.on('setGreeting', function(greeting) { ... });
+  // ```
+  //
+  // Wildcards and scopes are avaliable. The delimiter is `:`.
+  // The event name 'echo:*' is valid for `.emit('plugin:ping:echo:somebody')`.
+  // And `*` is to match any.
+  //
   this.on('*', function(txt) {
     // each message sended to me
-    _this.greeting = txt;
-  });
+    this.greeting = txt;
+  }.bind(this));
 
 }
 
+/**
+ * Override method
+ *   called when plugin destroied
+ */
 Ping.prototype.destroy = function() {
   this.logger('destroy ping plugin', arguments);
 };
 
-Ping.config = require('./config');
+/**
+ * Custom method
+ */
+Ping.prototype.ping = function() {
+  this.logger('-->', arguments);
+};
+
 
 module.exports = Ping;
