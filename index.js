69bf34a107e618a2a62dd3dd2e2c27cf403d5cea
ijse <i@ijser.cn>
2015-04-20 19:36:34 +0800

    redesign

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 
-function Ping(config, hub, router, log) {
+function Ping(config) {
 
   var _this = this;
   _this.greeting = config.greeting;
 
-  hub.once('plugin-init', function() {
-
-    router.get('/', function(req, res) {
-      res.end('Pong! ' + _this.greeting + '\n');
-    });
-
+  this.route.get('/', function(req, res) {
+    res.end('Pong! ' + _this.greeting + '\n');
   });
 
-  hub.on('plugin:ping:*', function(txt) {
+  this.on('setGreeting', function(txt) {
+    // each message sended to me
     _this.greeting = txt;
-
-    log('greeting set to ', txt);
   });
 
-  hub.once('destroy', function() {
-    // do some clean job
-    log('plugin ping is on destroy');
+  this.once('destroy', function() {
+    // destroy
+    this.logger('destroy ping plugin', arguments);
   });
 
 }
 
-// default configs export to datahub
+Ping.prototype.destroy = function() {
+  this.logger('destroy ping plugin', arguments);
+};
+
 Ping.config = require('./config');
 
 module.exports = Ping;
